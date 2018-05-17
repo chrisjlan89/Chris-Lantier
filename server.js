033d19eaf2be10c9cb4ac022c6827db05918346c
Chrisjlan89 <chrisj17@gmail.com>
2018-05-17 17:53:55 -0400

    header, body, and card chnages

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require ("express");
+const path = require ("path");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+//const routes = require("./routes/api/routes")
+//const ratingRoutes = require("./routes/api/ratingRoutes")
+const PORT = process.env.PORT || 8080;
+
+
+// app.use(express.static("./client/build"));
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+// Serve up static assets
+
+// Add routes, both API and view
+//app.use(routes);
+
+app.use(express.static(path.join(__dirname, "client")))
+// ... other app.use middleware setups
+//app.use(express.static(path.join(__dirname, "client", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+//app.get("*", (req, res) => {  
+ //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//});
+
+app.listen(PORT, function(){
+    console.log(`Server now on port ${PORT}`)
+})
+
+
+
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+ // mongoose.set('debug', false) // enable logging collection methods + arguments to the console
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/GamingGurus"
+
+);
+
+
